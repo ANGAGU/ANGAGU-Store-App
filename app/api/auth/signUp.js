b538d86988b7460d24290d1785df5051de61ba9f
Leejunho <lhsljh123@ajou.ac.kr>
2021-05-02 09:51:25 +0900

    feat: Add Signup API #2.2

---
+++ app/api/auth/signUp.js
@@ -0,0 +1,16 @@
+// Create Token
+import api from '..';
+
+export default async (phone, email, password) => {
+    try {
+        const {data: token} = await api.post(`/customer/signup`, {
+            phone,
+            email,
+            password
+        });
+        return token;
+    } catch (error) {
+        console.log(error, error.response);
+        return false;
+    }
+};
